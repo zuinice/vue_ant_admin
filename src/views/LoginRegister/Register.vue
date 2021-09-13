fa7e61d7482745fc58e8cff860f0912217a82d7e
zuinice <1070750171@qq.com>
2021-09-13 11:27:45 +0800

    test git

---
+++ src/views/LoginRegister/Register.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "VueAntAdminRegister",
+
+  components: {},
+
+  directives: {},
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
